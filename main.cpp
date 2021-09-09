d57a793a00ec6634e3180ee4b11e3027a0e9a767
Keith Lancaster <keithlancaster@unr.edu>
2021-09-09 08:23:43 -0700

    initial version

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+int main() {
+    int number;
+    std::cout << "Enter a number greater than 0:";
+    std::cin >> number;
+    std::cout << "You entered " << number << std::endl;
+    return 0;
+}

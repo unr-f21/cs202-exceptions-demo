f5a9e4cbb16b51c54d730cc3c4fffcbf33dc358e
Keith Lancaster <keithlancaster@unr.edu>
2021-09-09 08:36:23 -0700

    bad exception

--- main.cpp
+++ main.cpp
@@ -6,6 +6,17 @@ int main() {
     int number;
     std::cout << "Enter a number greater than 0:";
     std::cin >> number;
-    std::cout << "You entered " << number << std::endl;
+    try {
+        if (number < 1) {
+            throw "I said enter a number greater than 0";
+        }
+        std::cout << "You entered " << number << std::endl;
+    }
+    catch(const char* exception) {
+        if(strcmp("I said enter a number greater  than 0","I said enter a number greater than 0") == 0){
+            std::cout << "Bad number entered. Bye" << std::endl;
+            exit(1);
+        }
+    }
     return 0;
 }

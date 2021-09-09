4e9cd5bbe3d8117da11e790470addc2070a801d2
Keith Lancaster <keithlancaster@unr.edu>
2021-09-09 11:45:34 -0700

    using runtime errors

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 
 
+bool makeDatabaseConnection(){
+    //throw std::runtime_error("No database connection");
+    return true;
+}
+
+
+int retrieveDivisorFromDatabase(){
+    bool connection = makeDatabaseConnection();
+    int divisor = 0; // for this demo, pretend that the 3 came from the db
+    if(divisor <= 0)
+        throw std::runtime_error("Data in database is invalid");
+    return 0;
+}
+
 
 int main() {
-    int number;
+    float number;
     std::cout << "Enter a number greater than 0:";
     std::cin >> number;
     std::cout << "You entered " << number << std::endl;
+    try {
+        int divisor = retrieveDivisorFromDatabase();
+        int result = number / divisor;
+        std::cout << "The result is " << result << std::endl;
+    }
+    catch(std::runtime_error &error){
+        std::cout << error.what() << std::endl;
+        std::cout << "Program Exiting" << std::endl;
+        exit(1);
+    }
+
+
     return 0;
 }
+
+

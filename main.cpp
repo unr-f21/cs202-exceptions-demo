1e0695c218147d60413d67646e35358fc6487fff
Keith Lancaster <keithlancaster@unr.edu>
2021-09-09 09:46:53 -0700

    using asserts

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 
 int main() {
     int number;
+    int divisor = 0;
     std::cout << "Enter a number greater than 0:";
     std::cin >> number;
     std::cout << "You entered " << number << std::endl;
+    assert(divisor > 0);
+    std::cout << divisor << std::endl;
+    std::cout << number/ divisor << std::endl;
     return 0;
 }
